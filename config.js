9dcc2adfa91cb56041c0acc71f62006eedb4b64c
Yowazupnotu <paritosh.mehta05@gmail.com>
2026-01-28 21:00:17 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
 const CONFIG = {
     // Your Valentine's name that will appear in the title
     // Example: "Jade", "Sarah", "Mike"
-    valentineName: "Jade",
+    valentineName: "Nishku",
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
@@ -23,35 +23,35 @@ const CONFIG = {
     questions: {
         first: {
             text: "Do you like me?",                                    // First interaction
-            yesBtn: "Yes",                                             // Text for "Yes" button
-            noBtn: "No",                                               // Text for "No" button
-            secretAnswer: "I don't like you, I love you! ❤️"           // Secret hover message
+            yesBtn: "Yes!",                                             // Text for "Yes" button
+            noBtn: "No 😡",                                               // Text for "No" button
+            secretAnswer: "I dont like you, I LOVE YOUU! 💖💘💗"           // Secret hover message
         },
         second: {
-            text: "How much do you love me?",                          // For the love meter
-            startText: "This much!",                                   // Text before the percentage
+            text: "How MUCH do you love me huh? 😡😡",                          // For the love meter
+            startText: "This much 😝",                                   // Text before the percentage
             nextBtn: "Next ❤️"                                         // Text for the next button
         },
         third: {
-            text: "Will you be my Valentine on February 14th, 2025? 🌹", // The big question!
-            yesBtn: "Yes!",                                             // Text for "Yes" button
-            noBtn: "No"                                                 // Text for "No" button
+            text: "Will you be my Valentine on February 14th, 2026? 🌹", // The big question!
+            yesBtn: "YES OFC WYM?!",                                             // Text for "Yes" button
+            noBtn: "No 😡"                                                 // Text for "No" button
         }
     },
 
     // Love meter messages
     // They show up depending on how far they slide the meter
     loveMessages: {
-        extreme: "WOOOOW You love me that much?? 🥰🚀💝",  // Shows when they go past 5000%
-        high: "To infinity and beyond! 🚀💝",              // Shows when they go past 1000%
-        normal: "And beyond! 🥰"                           // Shows when they go past 100%
+        extreme: "MY BABY LOVES ME SO MUCH YAYAYAYAYAYAYAYAYAYAYAY 🥰😍😝😘",  // Shows when they go past 5000%
+        high: "OML ITNA SAARA PYAAR? 🥰🥰",              // Shows when they go past 1000%
+        normal: "Good itna to banta h"                           // Shows when they go past 100%
     },
 
     // Messages that appear after they say "Yes!"
     celebration: {
-        title: "Yay! I'm the luckiest person in the world! 🎉💝💖💝💓",
-        message: "Now come get your gift, a big warm hug and a huge kiss!",
-        emojis: "🎁💖🤗💝💋❤️💕"  // These will bounce around
+        title: "GGZ! Im genuinely THE LUCKIEST PERSON IN THE WORLD YAYAYAYAYAYAY! 🎉💝💖💝💓",
+        message: "Now gimme a hug and so many kissi and chumma chaatis that I overload 🥰🥰🥰 I LOVE YOU BABYYYYYYYY 🥰🥰",
+        emojis: "🎁🥰💖😍😘😝💝💋❤️💕"  // These will bounce around
     },
 
     // Color scheme for the website
